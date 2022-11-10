b48d17ac90401278e6c78423aefa500b99e9cb70
devzota <111525617+devzota@users.noreply.github.com>
2022-11-10 15:19:04 +0700

    đổi sang domain mới
    Hào

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -17,7 +17,7 @@ if (route.params.slug) {
     id = id[0];
   }
 
-  const apiUrl = `https://trend.newcarsz.com/wp-json/wp/v2/posts?slug=${id}`;
+  const apiUrl = `https://trend.dx1st.beauty/wp-json/wp/v2/posts?slug=${id}`;
   const response = await fetch(apiUrl);
   const json = await response.json();
   //console.log(data);
