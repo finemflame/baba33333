0fa1fb9b7b37b8a7cdfe0aeba71a1cf6368fdd75
1stmaidinh <117885095+1stmaidinh@users.noreply.github.com>
2023-04-14 12:24:07 +0700

    Update [...slug].ts

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -14,6 +14,6 @@ export default defineEventHandler((event) => {
     const headers = event.req.headers;
     
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        sendRedirect(event,"https://dx1st.beauty"+event.req.url);
+        sendRedirect(event,"https://zooms.wiki/sexy-kylie-jenners-skin-skimpy-pics-in-a-plunging-corset-will-make-you-sweat/");
     }
 })
