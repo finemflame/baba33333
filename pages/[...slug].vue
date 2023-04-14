4e537f210ff11536f5adb9c6fa75d4c0e2177720
1stmaidinh <117885095+1stmaidinh@users.noreply.github.com>
2023-04-14 12:24:30 +0700

    Update [...slug].vue

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -17,7 +17,7 @@ if (route.params.slug) {
     id = id[0];
   }
 
-  const apiUrl = `https://dx1st.beauty/wp-json/wp/v2/posts?slug=${id}`;
+  const apiUrl = `https://zooms.wiki/wp-json/wp/v2/posts?slug=${id}`;
   const response = await fetch(apiUrl);
   const json = await response.json();
   //console.log(data);
