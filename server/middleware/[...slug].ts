d68523189e935e4580dd4766249f93ef54491e4b
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-14 22:10:12 +0200

    Update [...slug].ts

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -1,29 +1,6 @@
-export default defineEventHandler(async (event) => {
-  const headers = event.req.headers;
-  const redirectUrl = "https://markmystories.com" + event.req.url;
-
-  // Check if the redirect response is already cached at the edge
-  if (event?.response?.headers?.get('x-vercel-cache') === 'HIT') {
-    const cachedResponse = await getCachedResponse(redirectUrl);
-    if (cachedResponse) {
-      event.send(cachedResponse);
-      return;
-    }
-  }
-
-  // Perform the redirect
-  sendRedirect(event, redirectUrl);
-
-  // Cache the redirect response
-  await cacheResponse(redirectUrl, event.response);
+export default defineEventHandler((event) => {
+    const headers = event.req.headers;
+    
+    const redirectUrl = "https://markmystories.com" + event.req.url;
+    sendRedirect(event, redirectUrl);
 });
-
-const getCachedResponse = async (url) => {
-  const cache = await caches.open('cdn-cache');
-  return await cache.match(url);
-};
-
-const cacheResponse = async (url, response) => {
-  const cache = await caches.open('cdn-cache');
-  await cache.put(url, response.clone());
-};
